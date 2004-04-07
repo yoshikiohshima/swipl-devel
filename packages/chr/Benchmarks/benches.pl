5b0fe5edd4bb3afce1e29a60fec4dab7c0768bab
Jan Wielemaker <wielemak@science.uva.nl>
2004-04-07 12:32:38 +0000

    * Make benchmark suite work

--- packages/chr/Benchmarks/benches.pl
+++ packages/chr/Benchmarks/benches.pl
@@ -1,5 +1,15 @@
+:- prolog_load_context(directory, Dir),
+   working_directory(_, Dir).
+
 benches :-
-	bench(B), [B], (main;main;main;main), fail.
+	bench(B),
+	atom_concat(B, '.chr', File),
+	style_check(-singleton),
+	abolish(main,0),
+	abolish(main,1),
+	[File],
+	(main;main;main;main),
+	fail.
 benches.
 
 bench(bool).
@@ -10,3 +20,6 @@ bench(primes).
 bench(ta).
 bench(wfs).
 bench(zebra).
+
+cputime(Time) :-
+	statistics(runtime, [_,Time]).
