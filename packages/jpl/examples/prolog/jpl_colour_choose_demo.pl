c2a7413b9555f5d161330f7031f370a434bdff1e
Jan Wielemaker <wielemak@science.uva.nl>
2004-05-11 16:07:04 +0000

    * Prolog demo files

---
+++ packages/jpl/examples/prolog/jpl_colour_choose_demo.pl
@@ -0,0 +1,24 @@
+% shoes a JColorChooser dialog, on top of a (necessary) JFrame,
+% and awaits OK/Cancel click
+
+jpl_colour_choose_demo :-
+	jpl_new( 'javax.swing.JFrame', ['frame with dialog'], F),
+	jpl_call( F, setLocation, [400,300], _),
+	jpl_call( F, setSize, [400,300], _),
+	jpl_call( F, setVisible, [@(true)], _),
+	jpl_call( F, toFront, [], _),
+	jpl_call( F, getContentPane, [], CP),
+	jpl_get( 'java.awt.Color', pink, Pink),
+	jpl_call( 'javax.swing.JColorChooser', showDialog, [CP,'pick a colo(u)r',Pink], C),
+	jpl_call( F, dispose, [], _),
+	(	C == @(null)
+	->	write( 'you cancelled')
+	;	write( 'you chose '), write( C)
+	),
+	nl.
+
+
+% this directive runs the above demo
+
+:- jpl_colour_choose_demo.
+
