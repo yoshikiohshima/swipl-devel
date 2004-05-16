36fd563315464f5f884827ab6781844888623f82
Jan Wielemaker <wielemak@science.uva.nl>
2004-05-16 12:23:59 +0000

    * Ongoing work

--- packages/jpl/examples/java/Family/compile.bat
+++ packages/jpl/examples/java/Family/compile.bat
@@ -1,5 +1,7 @@
 @rem this assumes that jpl.dll is somewhere on your PATH
 @rem and that jpl.pl is in your Prolog library
-javac -deprecation -classpath "..\..\..\jpl.jar;." Family.java
+call ..\env.bat
+
+javac -deprecation Family.java
 @pause
 
---
+++ packages/jpl/examples/java/env.bat
@@ -0,0 +1,9 @@
+@rem Set variables needed by the examples
+@echo off
+
+set SWI_PROLOG_HOME=C:\Program Files\pl
+set JAVA_HOME=C:\j2sdk1.4.2_04
+set PATH=%PATH%;%SWI_PROLOG_HOME%\bin;%JAVA_HOME%\bin
+set CLASSPATH=%CLASSPATH%;%SWI_PROLOG_HOME%\lib
+
+echo %CLASSPATH%
