10919be099e7eec72eaccb6b97e9d49cac143c4d
Markus Triska <markus.triska@gmx.at>
2014-11-07 18:05:37 +0100

    keep track with master

--- library/sandbox.pl
+++ library/sandbox.pl
@@ -445,14 +445,12 @@ safe_primitive(_ =.. _).
 safe_primitive(system:compound_name_arity(_,_,_)).
 safe_primitive(system:compound_name_arguments(_,_,_)).
 safe_primitive(copy_term(_,_)).
-safe_primitive(system:copy_term_nat(_,_)).
 safe_primitive(system:duplicate_term(_,_)).
 safe_primitive(system:copy_term_nat(_,_)).
 safe_primitive(numbervars(_,_,_)).
 safe_primitive(system:term_hash(_,_)).
 safe_primitive(system:term_hash(_,_,_,_)).
 safe_primitive(system:variant_sha1(_,_)).
-
 
 					% dicts
 safe_primitive(system:is_dict(_)).
