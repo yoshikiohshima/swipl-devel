930a1913a3c3d95d492f68519f3d47935c78367c
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2016-01-25 10:02:52 +0100

    Douglas cookie trick

--- boot/attvar.pl
+++ boot/attvar.pl
@@ -59,15 +59,44 @@ map_goals([G|Gs]):-
 	call(G),
 	map_goals(Gs).
 
+% nop/1 is for disabling code while staying in syntax
+system:nop(_).
+
+make_var_cookie(Var,VarID:SAtts):- assertion(attvar(Var)),
+   del_attr(Var,cookie), get_attrs(Var,Atts),
+   format(string(VarID),'~q',[Var]),
+   format(string(SAtts),'~q',[attrs(Var,Atts)]),
+   put_attr(Var,cookie,VarID).
+
+cookie:verify_attributes(_,_,[]).
+
+check_var_cookie(Var,FirstID:Expect):-
+  assertion(get_attr(Var,cookie,LastVarID)), % cookie is missing then something trampled this var (this is to decide how much to panic)
+   make_var_cookie(Var,VarID:SAtts),
+   nop((Expect==SAtts->true;print_message(trace,format('~N~q~n',[Expect==SAtts])))),
+   ((FirstID==VarID)
+    ->true;
+     (backtrace(30),
+      (LastVarID==FirstID-> Type = warning ; Type = error),  % detect between a shifts maybe vs a attvar bwing overwritten 
+       print_message(Type,format('~N~q~n',[Expect==SAtts])),
+       set_prolog_flag(access_level,system), % ensures trace durring wakeup
+       % leaving trace nop'ed out so we can run make check or other things easier
+       nop(trace), % someomes can just leap here since there will be false postives
+       nop(throw(Expect==SAtts)))),!.
+
 %%	collect_all_va_goal_lists(+KernelWakeups)//
 %
 %	Run the verify_attributes/3 unify hook for attributes on Attvar
 
 collect_all_va_goal_lists([]) --> [].
 collect_all_va_goal_lists(wakeup(Var, Att3s, Value, Rest)) -->
+   { make_var_cookie(Var,Cookie)},  
         ['$attvar_assign'(Var,Value)],
 	collect_va_goal_list(Att3s, Var, Value),
-        collect_all_va_goal_lists(Rest).
+        {check_var_cookie(Var,Cookie)},
+        collect_all_va_goal_lists(Rest),
+        /* Still no goals should ahve been ran yet */
+        {check_var_cookie(Var,Cookie)}.
 
 
 %%	collect_va_goal_list(+Att3s, +Var, +Value, -Goals)
